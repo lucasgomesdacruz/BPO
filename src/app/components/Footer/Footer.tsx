64a5b0610719ff6f4271dc9e828a9d69de46db38
lucasgomesdacruz <lucaass1997rp@gmail.com>
2026-04-11 18:56:16 -0300

    feat: create component footer

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+
+    return (
+        <div className={styles.footer_container}>
+            <h1>Em Desenvolvimento</h1>
+        </div>
+    )
+}
+
+export default Footer
