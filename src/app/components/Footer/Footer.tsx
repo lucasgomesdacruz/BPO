16e87f05fd047f9087de75edda3f5467a1e208d8
lucasgomesdacruz <lucaass1997rp@gmail.com>
2026-05-03 17:31:44 -0300

    feat: add footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,8 +4,31 @@ const Footer = () => {
 
     return (
         <div className={styles.footer_container}>
-            <h1>Em Desenvolvimento</h1>
+            <div className={styles.footer_info}>
+                <h3>
+                    Diego Consultoria
+                </h3>
+                <p>
+                    Soluções completas em Departamento Pessoal para sua empresa. Atuamos com ética, responsabilidade e foco em resultados.
+                </p>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+            <div>
+                <p>Contato</p>
+                <ul>
+                    <li><a href="mailto:contato@diegoconsultoria.com">contato@diegoconsultoria.com</a></li>
+                    <li><a href="tel:+551112345678">(11) 1234-5678</a></li>
+                </ul>
+            </div>
         </div>
+
     )
 }
 
