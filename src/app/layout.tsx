58bb2d5f20f4b52fb9cad7686dd57bebaaa499ba
lucasgomesdacruz <lucaass1997rp@gmail.com>
2026-07-11 18:46:22 -0300

    feat: adiciona carrosel para seção de contatos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,7 @@ import { PrimeReactProvider } from "primereact/api";
 import "./globals.css";
 
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-// import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-// import "primeflex/primeflex.css";
 
 
 import Header from "./components/Header/Header";
